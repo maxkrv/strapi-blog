c74e162de9328a4fe9e901b2109e84e657d12ebf
max_krv <maxkr1203@gmail.com>
2022-12-18 23:11:46 +0200

    initial commit

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CssBaseline } from "@mui/material";
+
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</>
+	);
+}
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import type { NextPage } from "next";
+import { Button } from "@mui/material";
+
+const Index: NextPage = () => {
+	return (
+		<div>
+			<Button variant="contained">hello</Button>
+		</div>
+	);
+};
+
+export default Index;
